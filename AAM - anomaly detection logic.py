366b9928ff780a87880ea04c8e8c004cfabb14c2
richardsonlorraine <richardsonlorraine@users.noreply.github.com>
2026-07-26 22:18:11 +0100

    Delete AAM - anomaly detection logic.py

--- AAM - anomaly detection logic.py
+++
@@ -1,4 +0,0 @@
-# Simple anomaly detection logic
-residuals = y_test - y_pred
-std_res = np.std(residuals) # Flag anomalies where error is 3 standard deviations
-anomalies = np.where(np.abs(residuals) 3 * std_res)[0]
