1003995cf8e790364da5bd90f1f95ac81bbed4cf
richardsonlorraine <richardsonlorraine@users.noreply.github.com>
2026-07-26 22:21:07 +0100

    Delete project 3 -  total variance.py

--- project 3 -  total variance.py
+++
@@ -1,5 +0,0 @@
-# Calculate total variance
-total_var = np.sum(sorted_eigenvalues)
-# Calculate ratio per component
-explained_variance_ratio = sorted_eigenvalues / total_var
-print(fVariance explained by first 2 components: np.sum(explained_variance_ratio[:2]):.2%)
